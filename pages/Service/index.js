72da6c15ed2a57a6f566c42c23f63ad23d67289a
Duy Huynh <duyhuynh6508@gmail.com>
2023-05-26 12:54:20 +0700

    change index

--- pages/Service/index.js
+++ pages/Service/index.js
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
-import Link from "next/link";
-import Image from 'next/image';
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Insurance from "@/components/Insurance";
+import Review from "@/components/Review";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import ContactForm from "@/components/ContactForm";
+import styles from "@/styles/Home.module.css";
 
-const Service = () => {
 
-    return (
-      <div>
-        Service
+export default function Service() {
+  return (
+    <>
+      <div className={styles.main}>
+        <Header headerName="Service"/>
+        <Navbar navbarSelected="Service"/>
+        <Hero />
+        <Insurance />
+        <Review />
+        <ContactForm/>
+        <Footer />
       </div>
-    );
-  };
-  
-  export default Service;
+    </>
+  );
+}
